42315b710b07a8bde881d7832bb3d6003757c7df
Hyun Seong Kim <rlagustjd2684@gmail.com>
2024-12-07 17:57:48 +0900

    11582번 문제 1차시도 실패

---
+++ src/num11582.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class num11582 {
+    static int[] arr;
+    static int[] divide(int[] x){
+
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        arr = new int[N];
+        for(int index=0; index<N; index++){
+            arr[index] = sc.nextInt();
+        }
+
+        int k = sc.nextInt();
+
+
+    }
+}
