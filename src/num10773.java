30eca46f1994c878b75bde315da210fa490a0af7
Hyun Seong Kim <rlagustjd2684@gmail.com>
2024-12-11 18:50:12 +0900

    구현-제로-조건문에서 else중요

--- src/num10773.java
+++ src/num10773.java
@@ -12,6 +12,8 @@ public class num10773 {
         Stack<Integer> stack = new Stack<Integer>();
 
         int K = sc.nextInt();
+        int result=0;
+
         /*
         K만큼 반복하여 스택구조에 숫자를 넣는데
         0일경우 스택에 있는 숫자를 꺼낸다. 0이 아닐경우 스택에 숫자를 넣는다.
@@ -19,12 +21,17 @@ public class num10773 {
          */
         for(int i=0; i<K; i++){
             int num=sc.nextInt();
-                if(num>0){//입력되는 정수는 0아니면 모두 다 양수이기에
+                //입력되는 정수는 0아니면 모두 다 양수이기에
+                if(num>0)   {
                     stack.push(num);
                 }
+                //스택이 비어있는 경우에 pop()을 한다면 에러가 발생한다.(발견하지 못했던 문제점)
+                if(stack.isEmpty())    {
+                    continue;
+                }
+                //입력받은 정수가 0이고 스택이 비어있지 않으니 꺼내어준다.
                 stack.pop();
         }
-        int result=0;
         for(int j=0;j<stack.size();j++){
             result+=stack.pop();
         }
