bf9007b40e4788e73f0433e114c7bf64f4b6eda1
Hyun Seong Kim <rlagustjd2684@gmail.com>
2024-12-17 21:35:55 +0900

    Comparator인터페이스를 활용한 순서 정의

--- src/greed/num1931.java
+++ src/greed/num1931.java
@@ -3,28 +3,59 @@ package greed;
 import java.util.*;
 
 /*
-사용시간표와 동일한 int배열을 만든 뒤 각 배열에는 초기값으로 0으로 세팅
-사용 하는 시간을 인덱스라고 생각하고 해당 인덱스에 값을 넣는다.
-하나의 회의가 종료되면 1을 추가하여 다음 회의가 시작될때 int배열에 값에다가는 추가된 값을 넣는다.
-그리디는 현재 상황에서 제일 최선의 선택을 고르는것이다. 이 경우 최대한 많은 회의를 진행해야하니 짧은 회의시간을 갖는것이 가장 베스트일거 같다.
-시작시간부터 끝나는 시간까지는 사용이 불가능하다.
-시작,끝나는 시간은 범위가 0부터 2^31까지이다. 편의를 위해 시작시간과 끝나는 시간을 한 세트로 묶어서 표현하겠다.
-입력받은 시간들중에서 끝나는 시간에서 시작시간을 빼서 차이가 가장 적은 순서대로 배열한 뒤
-{시작시간,끝나는시간}을 한 개씩 꺼내어 boolean배열로 사용을 표시
-int배열에는 사용회수를 나타내는 cnt를 사용하여 회의 사용 횟수를 기억한다.
+최대한 많은 회의실을 사용하기 위해서는 짧은 회의시간을 갖는 타임을 먼저 선택해야한다.
+겹치는 시간대가 있는경우 종료되는 시간을 기준으로
+회의시간이 짧은걸 선택한다.
+그리디 알고리즘은 현재 시점에서 최적의 선택을 하면 전체에서도 최적의 결과를 얻을 수 있다는 알고리즘이다.
+그렇다면 이 문제에서 현재 시점에서 최적의 선택은 종료시간이 가장 빠른 회의를 선택해야 남은 시간을 최대로 확보 할 수 있다.
  */
+class MeetingRoom implements Comparable<MeetingRoom>{
+    int startTime;
+    int finishTime;
 
+    public MeetingRoom(int startTime, int finishTime) {
+        this.startTime = startTime;
+        this.finishTime = finishTime;
+    }
+
+    @Override
+    public String toString() {
+        return "MeetingRoom " +
+                "startTime=" + startTime +
+                ", finishTime=" + finishTime ;
+    }
+
+    @Override
+    public int compareTo(MeetingRoom o2) {
+        /*
+        종료시간이 같을 경우 시작시간이 종료시간과 가까운 시간이 곧 사용시간이 짧은 것을 의미한다.
+        차이를 구해서 음수가 나올경우 this가 사용시간이 긴것이고
+        양수가 나올경우 this가 사용시간이 짧을것이다.
+        종료시간이 같지 않을 경우에는 종료시간이 가장 짧은 것이 나머지 시간의 극대화 하여 최적의 해를 찾게 해준다.
+         */
+        if(this.finishTime == o2.finishTime) return this.startTime - o2.startTime;
+        return finishTime - o2.finishTime;
+    }
+}
 public class num1931 {
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        //차이를 저장할 배열
-        int[] diff = new int[N];
+        MeetingRoom[] mr = new MeetingRoom[N];
 
         for(int i=0; i<N; i++) {
             int startTime = sc.nextInt();
             int finishTime = sc.nextInt();
+            mr[i] = new MeetingRoom(startTime,finishTime);
+        }
+        for(MeetingRoom m : mr){
+            System.out.println(m.toString());
+        }
+        Arrays.sort(mr);
+        System.out.println("----------------------------------------------");
+        for(MeetingRoom m : mr){
+            System.out.println(m.toString());
         }
     }
 }
