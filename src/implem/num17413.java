de2d6f45769d10d9c063a365d1c8635084603336
Hyun Seong Kim <rlagustjd2684@gmail.com>
2025-01-05 00:35:58 +0900

    구현-단어뒤집기-성공

--- src/implem/num17413.java
+++ src/implem/num17413.java
@@ -4,39 +4,48 @@ import java.io.*;
 import java.util.*;
 
 public class num17413 {
-    public static void main(String[] args)throws IOException{
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer token = new StringTokenizer(br.readLine());
+        String str = br.readLine();
+
+        boolean flag = false;
 
         StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
 
-        while( token.hasMoreTokens() ){
-            String str = token.nextToken();
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
 
-            Stack<Character> stack = new Stack<>();
-            Queue<Character> que = new LinkedList<>();
-
-            for(int i=0; i<str.length();){
-                char ch = str.charAt(i++);
-
-                if(ch == '<' ){
-                    que.add(ch);
-                    ch = str.charAt(i++);
-                    while(true){
-                        que.add(ch);
-                        ch = str.charAt(i++);
-                        if(ch =='>'){
-                            que.add(ch);
-                            break;
-                        }
-                    }
+            if(ch == '<'){
+                while(!stack.isEmpty()){
+                    sb.append(stack.pop());
                 }
-                while(!que.isEmpty()) {
-                    sb.append(que.poll());
+                flag = true;
+            }else if(ch == '>'){
+                sb.append(ch);
+                flag = false;
+                continue;
+            }
+
+            if(flag){
+                sb.append(ch);
+            }else if(!flag){
+                if(ch == ' '){
+                    while(!stack.isEmpty()){
+                        sb.append(stack.pop());
+                    }
+                    sb.append(' ');
+                }else{
+                    stack.push(ch);
+                }
+            }
+
+            if(i == str.length()-1){
+                while(!stack.isEmpty()){
+                    sb.append(stack.pop());
                 }
             }
         }
-
         System.out.println(sb);
     }
 }
